eded9de73b729628fb76a88452bd9c8d5a5517d9
chaerish <ccyy8432@naver.com>
2025-05-07 15:19:25 +0900

    init: create FastAPI main entry point and register pitch router

---
+++ main.py
@@ -0,0 +1,11 @@
+# main.py
+from fastapi import FastAPI
+from api.routers.pitch_router import router as pitch_router
+from dotenv import load_dotenv
+import os
+
+load_dotenv() #환경 변수 
+app = FastAPI(title="RFPilot")
+
+# 발표 평가 라우터 등록
+app.include_router(pitch_router, prefix="/pitch-evaluation", tags=["Pitch Evaluation"])
