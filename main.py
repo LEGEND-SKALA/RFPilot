f64a9e9762bd5e2e729e436fe29669751d571b92
chaerish <ccyy8432@naver.com>
2025-05-08 21:46:14 +0900

    feat: update main.py

--- main.py
+++ main.py
@@ -1,12 +1,8 @@
-# main.py
 from fastapi import FastAPI
-from api.routers.chunk_router import router as chunk_router
-from api.routers.router import router
 from dotenv import load_dotenv
-import os
+from api.routers.router import router
 
-load_dotenv() #환경 변수 
+load_dotenv()
 app = FastAPI(title="RFPilot")
 
-app.include_router(chunk_router)
-app.include_router(router, prefix="/pitch-evaluation", tags=["Pitch Evaluation"])
+app.include_router(router)
