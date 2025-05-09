63814f22ec0de84f3917487c2ef8493bf5851d69
류지혜/Jihye Ryu <domwis.IR@gmail.com>
2025-05-09 11:07:34 +0900

    fix: API Key load first before import router

--- main.py
+++ main.py
@@ -1,10 +1,18 @@
 from fastapi import FastAPI
 from dotenv import load_dotenv
+load_dotenv()
+
 from api.routers.router import router
-import os
 from fastapi.middleware.cors import CORSMiddleware
 
-load_dotenv()
 app = FastAPI(title="RFPilot")
 
 app.include_router(router)
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  # Or specify your Vue frontend URL
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
